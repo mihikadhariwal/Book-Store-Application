92a22bbddf1133c2bded17415f7cd8c3a8ba87ec
mihikadhariwal <mihikadhariwal@gmail.com>
2024-06-21 17:37:43 +0530

    database connection added

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,21 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
+
+mongoose
+  .connect(mongoURL)
+  .then(() => {
+    console.log("connection established");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB", err);
+  });
+
+app.get("/", (req, res) => {
+  res.send("welcome");
+});
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
